41942c2377b7cf11872419d94e2bb1da5d3d06ec
Administrator <admin@example.com>
2022-04-25 20:03:30 +0800

    first commit2

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func toWeirdCase(str string) string {
+	// => returns "WeIrD StRiNg CaSe"
+	// => returns "WeIrD StRiNg CaSe"
+	var result string = ""
+
+	indexOfSpace := -1
+	wordIndex := 0
+	for index, char := range str {
+		wordIndex =  index - (indexOfSpace + 1)
+
+		if wordIndex % 2 == 0 {
+			result += strings.ToUpper(string(char))
+		} else {
+			result += strings.ToLower(string(char))
+		}
+		if char == ' ' {
+			indexOfSpace = index
+		}
+	}
+	return result
+}
+
+
+
+
+
+
+
+func main() {
+	str1 := "Weird string case"
+	fmt.Println(toWeirdCase(str1))
+
+}
