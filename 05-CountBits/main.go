98c1ecd94a106d57b69d3fbeb58c07543b21977b
Administrator <admin@example.com>
2022-04-27 12:20:01 +0800

    add  kata exercise55

--- 05-CountBits/main.go
+++ 05-CountBits/main.go
@@ -9,23 +9,17 @@ func CountBits(number uint) int {
 	var bit string
 	var result int
 
-	for ;; {
-		if number % 2  == 0 {
-			bit += strconv.Itoa(0)
-		} else  {
+	for ; number > 0; number /= 2 {
+		if (number % 2) != 0 {
+			result++
 			bit += strconv.Itoa(1)
+		} else  {
+			bit += strconv.Itoa(0)
 		}
-		if number == 1 {
-			break
-		}
-		number = number / 2
 	}
 	//reverse but no need
 	for i := len(bit); i > 0; i-- {
 		fmt.Print(string(bit[i-1]))
-		if string(bit[i-1]) == "1" {
-			result++
-		}
 	}
 	fmt.Println()
 	return result
