2ee96de83a11190b30ba57781fcf51d8371567fc
Administrator <admin@example.com>
2022-04-27 11:54:18 +0800

    add  kata exercise5

--- 05-CountBits/main.go
+++ 05-CountBits/main.go
@@ -1,1 +1,36 @@
-package _5_CountBits
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func CountBits(number uint) int {
+	var bit string
+	var result int
+
+	for ;; {
+		if number % 2  == 0 {
+			bit += strconv.Itoa(0)
+		} else  {
+			bit += strconv.Itoa(1)
+		}
+		if number == 1 {
+			break
+		}
+		number = number / 2
+	}
+	//reverse but no need
+	for i := len(bit); i > 0; i-- {
+		fmt.Print(string(bit[i-1]))
+		if string(bit[i-1]) == "1" {
+			result++
+		}
+	}
+	fmt.Println()
+	return result
+}
+
+func main() {
+	fmt.Println(CountBits(19))
+}
